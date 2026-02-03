075c1382ec726bca9076130e89e0c1394283be95
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-02-03 14:14:27 -0300

    adicionando fotos usuarios

--- first-angular18-app/src/app/app.component.ts
+++ first-angular18-app/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component'; 
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';ferwfer
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular18-app';
+  title = 'first-angular18-app';de
 }
