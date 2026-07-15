7cbf2c4b0d66ed55f857449f7d740c55ae0c4c97
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-15 19:24:57 -0300

    aprendendo sobre tipagem

--- starting-project/01-starting-project/src/app/task/task.component.ts
+++ starting-project/01-starting-project/src/app/task/task.component.ts
@@ -6,10 +6,11 @@ import { DUMMY_USERS } from '../dummy-users';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [UserComponent],
+  imports: [],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({ required: true }) name!: string;
+  @Input() name2?: string | undefined;
 }
