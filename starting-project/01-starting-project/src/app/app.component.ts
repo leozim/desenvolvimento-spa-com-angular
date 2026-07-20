8d5186aff772318628b1c14eb27437aabc45162d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-20 20:32:24 -0300

    using for and if directive angular

--- starting-project/01-starting-project/src/app/app.component.ts
+++ starting-project/01-starting-project/src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TaskComponent } from './task/task.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
